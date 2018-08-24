48ceff46bef9ba79e210e4a9c153d5862b00cb48
Stanford C. Goss <gmixlpixl@gmail.com>
2018-08-24 14:58:55 -0400

    init commit

---
+++ main.js
@@ -0,0 +1,11 @@
+new Vue({
+  el: '#app',
+  data: {
+    ranks: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+    suits: ['♥', '♦', '♠', '♣'],
+    cards: [],
+  },
+  methods: {
+    displayInitialDeck() {},
+  }
+})
